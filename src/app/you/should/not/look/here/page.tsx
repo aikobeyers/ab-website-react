f9d7366c62cffb918ecff3653b3f8bdd7524f2c4
aikobeyers <aikobeyers@hotmail.com>
2024-11-20 16:14:39 +0100

    Show random message of hardcoded set

--- src/app/you/should/not/look/here/page.tsx
+++ src/app/you/should/not/look/here/page.tsx
@@ -1,9 +1,20 @@
 import style from './page.module.css';
 
+const quotes: Array<string> = [
+    'If you are seeing this, buy me snacks',
+    'You have to buy me a beer',
+    'Give me 5 bucks'
+];
+
+const getQuote = () => {
+    const index = Math.floor(Math.random() * quotes.length);
+    return quotes[index]
+}
+
 export default function Snacks() {
     return (
         <div className={style.wrapper}>
-            <span className={style.message}>If you are seeing this, buy me snacks</span>
+            <span className={style.message}>{getQuote()}</span>
         </div>
     );
 }
