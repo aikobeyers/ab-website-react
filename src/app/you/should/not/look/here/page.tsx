614d65a6bab355626d97e95a296270aff9e9e426
aikobeyers <aikobeyers@hotmail.com>
2024-11-21 16:22:49 +0100

    first hook usage

--- src/app/you/should/not/look/here/page.tsx
+++ src/app/you/should/not/look/here/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import style from './page.module.css';
+import {useEffect, useState} from "react";
 
 const quotes: Array<string> = [
     'If you are seeing this, buy me snacks',
@@ -14,14 +16,16 @@ const getQuote = () => {
     return quotes[index]
 }
 */
-
 export default function Snacks() {
-    console.log('pageload');
+    const [displayQuote, setDisplayQuote] = useState('')
+
     const index = Math.floor(Math.random() * quotes.length);
+    useEffect(() => setDisplayQuote(quotes[index]), [index]);
+
 
     return (
         <div className={style.wrapper}>
-            <span className={style.message}>{quotes[index]}</span>
+            <span className={style.message}>{displayQuote}</span>
         </div>
     );
 }
