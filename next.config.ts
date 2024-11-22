e0893e15381485e49e623c24353414bfd96fcc88
aikobeyers <aikobeyers@hotmail.com>
2024-11-22 16:45:54 +0100

    fetch data fro; api and show random quote

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/quotes',
+                destination: 'https://secret-message-api.netlify.app/.netlify/functions/server',
+            },
+        ];
+    }
 };
 
 export default nextConfig;
--- src/app/you/should/not/look/here/page.tsx
+++ src/app/you/should/not/look/here/page.tsx
@@ -2,27 +2,25 @@
 import style from './page.module.css';
 import {useEffect, useState} from "react";
 
-const quotes: Array<string> = [
+
+const quotesList: Array<string> = [
     'If you are seeing this, buy me snacks',
     'You have to buy me a beer',
     'Give me 5 bucks'
 ];
+const index = Math.floor(Math.random() * quotesList.length);
+export default function SecretMessage() {
+    const [displayQuote, setDisplayQuote] = useState('');
+    useEffect(() => {
+        fetch('/api/quotes')
+            .then((res) => res.json())
+            .then((data) => data.map((record) => record.quote))
+            .then((quotes) => {
+                console.log('state quotes', quotes, index);
+                setDisplayQuote(quotes[index])
+            })
 
-/*
-const getQuote = () => {
-    const index = Math.floor(Math.random() * quotes.length);
-    console.log(quotes);
-    console.log(index);
-    return quotes[index]
-}
-*/
-export default function Snacks() {
-    const [displayQuote, setDisplayQuote] = useState('')
-
-    const index = Math.floor(Math.random() * quotes.length);
-    useEffect(() => setDisplayQuote(quotes[index]), [index]);
-
-
+    }, [])
     return (
         <div className={style.wrapper}>
             <span className={style.message}>{displayQuote}</span>
